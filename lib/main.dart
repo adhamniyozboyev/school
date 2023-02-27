0b6f11c0674fb637b0eb5d3276d7d4f366d2976a
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-27 12:11:27 +0500

    Add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,55 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          alignment: Alignment.center,
+          height: 300,
+          width: 200,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [Icon(Icons.home_filled), Text('Classes')],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [Icon(Icons.calendar_today), Text('Calendar')],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Teaching'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [Icon(Icons.folder), Text('To review')],
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(onPressed: () {}, child: Text('Stream')),
-            TextButton(onPressed: (){}, child: Text('Classwork')),
-            TextButton(onPressed: (){}, child: Text('People')),
-            TextButton(onPressed: (){}, child: Text('Grades'))
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Stream',
+                  style: TextStyle(color: Colors.white),
+                )),
+            TextButton(
+                onPressed: () {},
+                child:
+                    Text('Classwork', style: TextStyle(color: Colors.white))),
+            TextButton(
+                onPressed: () {},
+                child: Text('People', style: TextStyle(color: Colors.white))),
+            TextButton(
+                onPressed: () {},
+                child: Text('Grades', style: TextStyle(color: Colors.white)))
           ],
         ),
       ),
